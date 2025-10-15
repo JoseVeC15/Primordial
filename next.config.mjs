7f9377651b2af393f7738c829457017c2b0f6b25
JoseVeC15 <joseverdun1@gmail.com>
2025-10-15 00:12:59 -0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true, // 🚫 desactiva revisión ESLint en builds
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
-
 export default nextConfig;
